c6001423f6b342c320f62c4aa12e98c56a413b0b
Junior Godoi <100797468+hard87@users.noreply.github.com>
2025-09-02 10:56:02 -0300

    Modificando o arquivo / JuniorGodoi

--- Spotifei-Back/Program.cs
+++ Spotifei-Back/Program.cs
@@ -1,3 +1,5 @@
 ﻿System.Console.WriteLine("André Medeiros Corrêa");
 System.Console.WriteLine("Guilherme Macedo Varela");
 System.Console.WriteLine("Marcos Vinicius Souza Tavares");
+System.Console.WriteLine("Adilson Do Amaral Godoi Junior");
+
