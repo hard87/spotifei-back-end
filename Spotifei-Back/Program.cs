ac1743600775423a82841f4fe7e0faefde09e4c1
Alexander Araldi <alexanderaraldi@gmail.com>
2025-09-02 12:49:57 -0300

    incluido meu nome no projeto spotifei-back-end

--- Spotifei-Back/Program.cs
+++ Spotifei-Back/Program.cs
@@ -2,4 +2,5 @@
 System.Console.WriteLine("Guilherme Macedo Varela");
 System.Console.WriteLine("Marcos Vinicius Souza Tavares");
 System.Console.WriteLine("Adilson Do Amaral Godoi Junior");
+System.Console.WriteLine("Alexander Araldi de Oliveira Filho");
 
