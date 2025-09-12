d7ebfd988b3c6a46690241a355ad7e2dc49b43bc
AlexAraldi <alexanderaraldi@gmail.com>
2025-09-12 19:01:34 -0300

    Adicionei os métodos e propriedades de todas as classes do Dal

---
+++ Spotifei-Back/Dal/PlaylistDao.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotifai_Back.DAL
+{
+    public class PlaylistDAO
+    {
+        private static List<Playlist> playlists = new List<Playlist>();
+        private static int nextId = 1;
+
+        public void Adicionar(Playlist playlist)
+        {
+            if (playlist == null) throw new ArgumentNullException(nameof(playlist));
+            playlist.Id = nextId++;
+            playlists.Add(playlist);
+        }
+
+        public List<Playlist> ObterTodos()
+        {
+            return new List<Playlist>(playlists);
+        }
+
+        public Playlist ObterPorId(int id)
+        {
+            return playlists.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool Atualizar(Playlist playlist)
+        {
+            if (playlist == null) return false;
+            var existing = ObterPorId(playlist.Id);
+            if (existing == null) return false;
+
+            existing.Nome = playlist.Nome;
+            existing.DataCriacao = playlist.DataCriacao;
+            existing.NumeroMusicas = playlist.NumeroMusicas;
+            existing.Usuario = playlist.Usuario;
+            existing.Musicas = playlist.Musicas;
+
+            return true;
+        }
+
+        public bool Remover(int id)
+        {
+            var playlist = ObterPorId(id);
+            if (playlist == null) return false;
+            playlists.Remove(playlist);
+            return true;
+        }
+    }
+}
---
+++ Spotifei-Back/Dal/UsuarioDao.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotifai_Back.DAL
+{
+    public class UsuarioDAO
+    {
+        private static List<Usuario> usuarios = new List<Usuario>();
+        private static int nextId = 1;
+
+        public void Adicionar(Usuario usuario)
+        {
+            if (usuario == null) throw new ArgumentNullException(nameof(usuario));
+            usuario.Id = nextId++;
+            usuarios.Add(usuario);
+        }
+
+        public List<Usuario> ObterTodos()
+        {
+            return new List<Usuario>(usuarios);
+        }
+
+        public Usuario ObterPorId(int id)
+        {
+            return usuarios.FirstOrDefault(u => u.Id == id);
+        }
+
+        public bool Atualizar(Usuario usuario)
+        {
+            if (usuario == null) return false;
+            var existing = ObterPorId(usuario.Id);
+            if (existing == null) return false;
+
+            existing.Login = usuario.Login;
+            existing.Senha = usuario.Senha;
+
+            return true;
+        }
+
+        public bool Remover(int id)
+        {
+            var usuario = ObterPorId(id);
+            if (usuario == null) return false;
+            usuarios.Remove(usuario);
+            return true;
+        }
+    }
+}
